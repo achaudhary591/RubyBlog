a952cc82c652038bc87ff510f0b1d1862c9c3d56
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-24 15:30:56 +0530

    added new migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_24_074918) do
+ActiveRecord::Schema.define(version: 2022_11_24_094841) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
