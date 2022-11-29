6779d91f71c6c9c442fbd9069dd3e4bcb4fb90d3
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-29 11:40:15 +0530

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-
-
   has_many :articles
 
   validates :username, presence: true,
@@ -16,6 +14,8 @@ class User < ApplicationRecord
              uniqueness: { case_sensitive: false },
              length: { maximum: 105 },
              format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 
 end
 
