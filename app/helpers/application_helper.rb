8c96167f720d5f109aeb7d04d7b00f5b1147049f
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-30 09:30:46 +0530

    made image picker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def gravatar_for(user, options = { size: 80})
+  def profile_image(user, options = { size: 80})
     url = "https://picsum.photos/200"
     image_tag(url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
