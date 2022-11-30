d6b3f39258b84dfe34ec09018732075e62cae621
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-30 17:28:59 +0530

    authenticate succesfully

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     image_tag(url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
